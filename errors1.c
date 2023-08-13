3182d54967da605c563f3d6a981048a4c55ea188
fouadslh001 <133231627+fouadslh001@users.noreply.github.com>
2023-08-13 14:15:48 +0100

    updated

---
+++ errors1.c
@@ -0,0 +1,153 @@
+#include "shell.h"
+
+/**
+ * _erratoi - will converts a string to an integer number
+ *
+ * @s: the string we need to be converted for
+ * Return: 0 if there no numbers in string, otherwise converted to a number.
+ *       -1 on error or fail the processe
+ */
+
+
+int _erratoi(char *s)
+{
+	int i = 0;
+	unsigned long int result = 0;
+
+	if (*s == '+')
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
+}
+
+/**
+ * print_error - will prints the error message
+ *
+ * @info: the parameter to enter and the return info of the struct
+ * @estr: string containing the specified error type we need
+ * Return: 0 if no numbers in string, or converted number anyway
+ *        -1 on error or fail processe.
+ *
+ */
+
+
+void print_error(info_t *info, char *estr)
+{
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
+}
+
+/**
+ * print_d - function that prints a decimal, (integer or number) of (base 10)
+ * @input: the input we need to enter
+ * @fd: the filedescriptor to write to for
+ *
+ *
+ * Return: numbers of the characters printed
+ */
+
+int print_d(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
+
+	return (count);
+}
+
+/**
+ * convert_number - the converter function will, clone of itoa
+ * @num: number we need
+ * @base: base we need to enter
+ * @flags: the argument flags
+ *
+ *
+ * Return: will return a string
+ */
+
+char *convert_number(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
+/**
+ * remove_comments - function will replaces, first instance of '#' with '\0'
+ * @buf: address of the string we need to be modifyed.
+ *
+ * Return: Always 0 means work sucssese;
+ */
+
+
+void remove_comments(char *buf)
+{
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
+}
